167a83e8c1d9dfb940ee405fd4223e6e3961521c
JeanneD4RK <mdecpr@gmail.com>
2024-07-26 21:03:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# CH340G Driver
+
+Chocolatey package config for CH340g driver, mainly used to program Arduino / ATmega PCBs
+
+Current version 3.8.2023.2
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'install\SETUP.EXE'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  file         = $fileLocation
+
+  softwareName  = 'ch340g-driver*'
+
+  checksum      = '2F6C540841884BE0F61C9A63A83D4CE68F2648E0EDD46AF78B0F49F88C45F7F2'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/U'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
